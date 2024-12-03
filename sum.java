1f3d2811e5e3951a706518843bc83084e1f787a8
pallavi thakur <thakurpallavi560@gmail.com>
2024-12-03 12:50:22 +0530

    files added

---
+++ sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class sum{
+    public static void main(String args[]){
+        System.out.println("Enter of numbers");
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int sum=a+b+c;
+        System.out.println(sum); 
+        sc.close();     
+    }
+}
